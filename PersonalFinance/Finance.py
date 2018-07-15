d7322319fe037d5aec9342adef29c7ba2b60d7f0
tuto193 <carlos.a.parra.f@gmail.com>
2018-07-15 19:33:34 +0200

    We will put all back on Dropbox

--- PersonalFinance/Finance.py
+++ PersonalFinance/Finance.py
@@ -5,19 +5,23 @@ This financing program is meant to be run manually every monday to keep track of
 and make sure that the hours are being paid accordingly
 """
 def exists( fileName ):
-    file = open( fileName +".txt", "r", encoding="utf-8" )
-    return file.readline == "Worker: " + fileName
+    with open( fileName +".txt", "r", encoding="utf-8" ) as file:
+        return file.readline == "Worker: " + fileName
 
 def get_worker_from_file( fileName ):
-    file = open( fileName+".txt", "r", encoding="utf-8")
-    worker = Worker.Worker(file.readline)
-    job = 
-    worker.set_job
+    with open( fileName+".txt", "r", encoding="utf-8") as file:
+        name = file.readline.split()
+        worker = Worker.Worker(name[1])
+        worker.job = file.readline
+
 
 if __name__ == "__main__":
     print("Welcome to your Financing App!")
     while True:
         workerName = input("What's your name?")
         if exists( workerName ):
+            print("Welcome back %s! It's nice to see you again." %workerName )
+        else:
+            print("Nice to meet you, %s. I will manage your finances" %workerName)
 
         
--- PersonalFinance/Job.py
+++ PersonalFinance/Job.py
@@ -3,22 +3,49 @@ class Job(object):
     Represents a Job, which is either paid
     hourly or monthly
     """
-    def __init__(self, name, hourly):
+    def __init__(self,name='A Job',hourly=9.49,holiday=9.49,night=9.49):
         self.name = name
         self.rate = hourly
-        self.month = []
+        self.rate_holiday = holiday
+        self.rate_night = night
+        self.night_hours = 0
+        self.holy_hours = 0
+        self.month = [[],[]]
         self.income = 0
 
     def get_monthly(self):
+        """
+        Get an approximation of what one would have earned this month.
+        It is just an approximation and it's also innaccurate, because
+        it takes (if any) the first 2 days of the month as special (either as night shifts
+        or as holiday shifts)
+        """
         total = 0
-        for i in len(1, self.month, 2):
-            hours = int(self.month[i])
+        nights = self.night_hours
+        holyday = self.holy_hours
+        subtract = 0
+        for i in len(self.month[1]):
+            hours = int(self.month[1][i])
             #account for unpaid breaks
             if hours >= 10:
                 hours -= 1
             elif hours >= 6:
                 hours -= 0.5
-            total += (hours*self.rate)
+            #account for special hours (inaccurate)
+            if nights > 0:
+                total += (nights*self.rate_night)
+                hours -= nights
+                nights = 0
+            elif holyday > 0:
+                total += (holyday*self.rate_holiday)
+                hours -= holyday
+                holyday = 0
+            # We don't work negative hours, but we do need to account for the negative hours
+            hours += subtract
+            if hours >= 0:
+                total += (hours*self.rate)
+            else:
+                subtract = hours
         return total
     def set__income(self, income):
         self.income = income
@@ -26,5 +53,19 @@ class Job(object):
         return self.get_monthly - self.income
     def ideal_mini_hours(self):
         return 450/self.rate
-
+    
+    def add_workday(self, date, hours):
+        self.month[0].append(date)
+        self.month[1].append(hours)
+    
+    def set_holyPay(self, rate):
+        self.rate_holiday = rate
+    
+    def set_nightPay(self, rate):
+        self.rate_night = rate
+    
+    def reset_month(self):
+        self.month = [[],[]]
+        self.holy_hours = 0
+        self.night_hours = 0
 
